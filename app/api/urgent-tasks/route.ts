c66cffe2a4e19118274ce43539587b720bff2df1
Bhatti, Hasoor (UG - Comp Sci & Elec Eng) <hb01395@surrey.ac.uk>
2026-09-18 05:02:11 +0100

    nasirat portal added

--- app/api/urgent-tasks/route.ts
+++ app/api/urgent-tasks/route.ts
@@ -77,6 +77,9 @@ async function getAreaCheck(
     RestrictedGroup | null =
     normalizedName.startsWith(
       "lajna"
+    ) ||
+    normalizedName.startsWith(
+      "nasirat"
     )
       ? "lajna"
       : normalizedName.startsWith(
